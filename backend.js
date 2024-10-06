06063b0c2af510ead840644db30b9de1c7804e56
Jason Li <83044757+Atxous@users.noreply.github.com>
2024-10-06 00:23:13 +0000

    update

--- backend.js
+++ backend.js
@@ -1,17 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const envelope = document.getElementById("envelope");
     const letter = document.getElementById("letter");
-    const nextPageButton = document.getElementById("nextPage");
   
-    // Handle envelope click to open
+    // Handle envelope click
     envelope.addEventListener("click", function() {
       envelope.classList.add("open");
       letter.classList.add("show-letter");
-    });
-  
-    // Handle "Next Page" button click to navigate
-    nextPageButton.addEventListener("click", function() {
-      window.location.href = "nextpage.html"; // Navigate to the next page
     });
   });
   
